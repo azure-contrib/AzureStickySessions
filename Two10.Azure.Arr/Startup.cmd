064948c36b763cf524c12d1050ffdaab3a16be75
Richard Astbury <richard.astbury@gmail.com>
2012-03-21 15:34:44 +0000

    Adding a custom app pool to fix issue #1. Thanks Simon!

--- Two10.Azure.Arr/Startup.cmd
+++ Two10.Azure.Arr/Startup.cmd
@@ -6,7 +6,7 @@ SET ID=20
 SET NAME="Default Web Site"
 SET PORT=80
 SET PHYSICAL_PATH=d:\inetpub\wwwroot
-SET APP_POOL="ASP.NET v4.0"
+SET APP_POOL="CustomAppPool"
 
 REM Discover the IP Address
 IPCONFIG |FIND "IPv4 Address" > %temp%\TEMPIP.txt
@@ -15,5 +15,6 @@ del %temp%\TEMPIP.txt
 set IP=%IP:~1%
 
 REM Configure IIS
+%systemroot%\system32\inetsrv\appcmd add apppool /name:%APP_POOL%
 %systemroot%\system32\inetsrv\appcmd add site /name:%NAME% /id:%ID% /bindings:http/%IP%:%PORT%: /physicalPath:%PHYSICAL_PATH%
 %systemroot%\system32\inetsrv\appcmd set site /site.name:%NAME% /[path='/'].applicationPool:%APP_POOL%
