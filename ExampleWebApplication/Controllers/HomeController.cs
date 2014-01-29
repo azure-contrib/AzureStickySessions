a480780dc5bd918b13b76abaaa7c07b4974d06c4
Richard Astbury <richard.astbury@gmail.com>
2014-01-29 10:31:55 +0000

    tidy up

---
+++ ExampleWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ExampleWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = string.Format("Hello from {0}", Environment.MachineName);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ExampleWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExampleWebApplication.MvcApplication" Language="C#" %>
